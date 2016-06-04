f0c740c430268b5adb6074ec11870146f9bb1886
Ben Wilcox <reccanti@gmail.com>
2016-06-04 00:51:20 -0400

    Initial Commit

---
+++ app/Http/Controllers/Chord/ChordController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Chord;
+
+use App\Http\Controllers\Controller;
+
+class ChordController extends Controller {
+    
+    /**
+     * This function parses the name of the chord 
+     * and then renders it on the page
+     */
+    public function parseChord ($name) {
+        $chord = explode('-', $name);
+        $suffix = '';
+        if (count($chord) > 1) {
+            if ($chord[1] == 'sharp') {
+                $suffix = '#';
+            } else if ($chord[1] == 'flat') {
+                $suffix = 'b';
+            }
+        }
+        return $chord[0] . $suffix . ' Chord';
+    }
+}
